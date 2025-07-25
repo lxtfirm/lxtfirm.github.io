639664007ec0863d0ed5e6d8cbb48350331de699
firm <firm>
2025-07-25 12:20:34 +0800

    Site updated: 2025-07-25 12:20:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/03/quick-sort/","2025/07/23/ext2/","2025/05/30/hello-world/","2025/06/03/扩容缓存区/"];function toRandomPost(){
+var posts=["2025/07/23/ext2/","2025/05/30/hello-world/","2025/06/03/quick-sort/","2025/06/03/扩容缓存区/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
